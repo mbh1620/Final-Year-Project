834dc10bc4ed92d26c0f340d3ed43b836e91ffbd
Matthew Haywood <43278160+mbh1620@users.noreply.github.com>
2022-11-17 19:26:32 +0000

    added FaceNormalTest.cpp
    Added file for testing FaceNormal

---
+++ FaceNormalTest.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "FaceNormal.h"
+
+int main(){
+
+	float a[3] = {0.5, 0.5, 0.5};
+	float b[3] = {3.2, 4.4, 5.5};
+	float c[3] = {6.4, 4.5, 3.4};
+
+	std::vector<float> normal = faceNormal(a, b, c);
+
+	if (normal[0] == 3 && normal[1] == 2 && normal[2] == 1){
+		std::cout << "Face Normal - Test 1 Passed!";
+	} else {
+		std::cout << "Face Normal - Test 1 Failed!";
+	}
+
+}
