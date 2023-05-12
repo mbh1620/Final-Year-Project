74e56579a62f1fb4b8661f37a172718ea91f9b9f
Matthew Haywood <43278160+mbh1620@users.noreply.github.com>
2023-05-12 11:48:52 +0100

    Delete CTestTestfile.cmake

--- CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/matthaywood/Desktop/Final Year Project/Final Year Code/Final-Year-Project
-# Build directory: /Users/matthaywood/Desktop/Final Year Project/Final Year Code/Final-Year-Project
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
